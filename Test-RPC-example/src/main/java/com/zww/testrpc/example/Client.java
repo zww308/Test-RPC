5a7d0ad2c1d3b3fcfab37e952bffb972715934a8
zww308 <1725529638@qq.com>
2021-02-21 15:14:24 +0800

    增加测试

---
+++ Test-RPC-example/src/main/java/com/zww/testrpc/example/Client.java
@@ -0,0 +1,21 @@
+package com.zww.testrpc.example;
+
+import com.zww.testrpc.client.RpcClient;
+
+/**
+ * @author zww
+ * @date 2021/2/21 15:02
+ */
+
+public class Client {
+
+    public static void main(String[] args) {
+        RpcClient client = new RpcClient();
+        CalcService service = client.getProxy(CalcService.class);
+        int r1 = service.add(1, 2);
+        int r2 = service.minus(10, 8);
+        System.out.println(r1);
+        System.out.println(r2);
+    }
+
+}
